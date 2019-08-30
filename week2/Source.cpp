2c0766416ebbc39f3934b4207f39e8b30796a763
fox5396 <foxdj@mymail.vcu.edu>
2019-08-29 21:02:41 -0400

    powerofx

--- week2/Source.cpp
+++ week2/Source.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
+	int x = 1;
+	while (x != 0)
+	{
+
+		cout << "Please enter a value" << endl;
+		cin >> x;
+
+		cout << "The square of the number you have entered is: " << pow(x, 2) << endl;
+	}
+	system("pause");
 	return 0;
 }
